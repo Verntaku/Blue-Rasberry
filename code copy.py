5ce78d30d52aa4e4af41e807d1740a8b9741dd70
Vern. <divastarkayla17@gmail.com>
2026-03-17 14:57:22 -0500

    update 1 firstScript

--- code copy.py
+++ code copy.py
@@ -1,1 +1,14 @@
 #This is a non AI Sesion insteace. Pracrice makes pefecrt. 
+import turtle
+import random
+import time
+
+def start():
+    turtle.speed(10) 
+    turtle.circle(100)
+    turtle.penup()
+    turtle.goto(-150, 0)
+    turtle.pendown()
+    turtle.circle(50)
+    
+turtle.
