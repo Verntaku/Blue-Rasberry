f7841a9f853ee5a36fbff8faeb2618ce9e99f2c0
Vern. <divastarkayla17@gmail.com>
2026-03-17 16:24:12 -0500

    Fully did the intro to the game!

--- code copy.py
+++ code copy.py
@@ -8,7 +8,9 @@ def start():
     turtle.circle(100)
     turtle.goto(0,50)
     turtle.circle(50)
+    turtle.penup()
+    turtle.goto(0,90)
     turtle.write("The number game", align="Center")
-    
+turtle.hideturtle()
 start()
 turtle.mainloop()
